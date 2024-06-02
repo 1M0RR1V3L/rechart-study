8aae4ab53a19a426ba8ac6d6c4f3b015d788ed41
1M0RR1V3L <lsxyz2017@gmail.com>
2024-06-02 20:45:30 -0300

    add area char legend, span, diferents colors

--- rechart-examples/src/components/AreaChart.jsx
+++ rechart-examples/src/components/AreaChart.jsx
@@ -1,5 +1,5 @@
 'use client'
-import { AreaChart, Area, ResponsiveContainer } from 'recharts'
+import { AreaChart, Area, ResponsiveContainer, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts'
 
 const productSales = [
     {
@@ -42,10 +42,40 @@ const AreaChartComponent = () => {
     return ( 
     <ResponsiveContainer width="100%" height="100%">
       <AreaChart width={500} height={400} data={productSales}>   
-        <Area dataKey="product1" />
+        <YAxis />
+        <YAxis dataKey="name" />
+        <CartesianGrid strokeDasharray="5 5" />
+        <Tooltip content={<CustomTooltip />}/>
+        <Legend />
+        <Area 
+          type="monotone"
+          dataKey="product1" 
+          stroke="#2363eb"
+          fill="#3b82f6"
+        />
+        <Area 
+          type="monotone"
+          dataKey="product2" 
+          stroke="#2363eb"
+          fill="#8b5cf6"
+        />
       </AreaChart>
     </ResponsiveContainer>
    
      );
+};
+
+const CustomTooltip = ({ active, payload, label }) => {
+    if (active && payload && payload.length) {
+      return (
+        <div className="p-4 bg-slate-900 flex flex-col gap-4 rounded-md">
+          <p className="text-medium text-lg">{label}</p>
+          <p className="text-sm text-blue-400">
+            Product 1:
+            <span className="ml-2">${payload[1].value}</span>
+          </p>
+        </div>
+      );
+    }
 };
 export default AreaChartComponent;
