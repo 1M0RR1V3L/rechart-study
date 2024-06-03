72236a89304152e14730a16ed00bc5fb38db79a7
Emanuel Avila <avilaemanueel@gmail.com>
2024-06-03 14:01:56 -0300

    update stuffs

--- rechart-examples/src/components/AreaChart.jsx
+++ rechart-examples/src/components/AreaChart.jsx
@@ -1,86 +1,92 @@
-'use client'
-import { AreaChart, Area, ResponsiveContainer, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts'
+import {
+  Area,
+  AreaChart,
+  CartesianGrid,
+  Legend,
+  ResponsiveContainer,
+  Tooltip,
+  YAxis,
+} from "recharts";
 
 const productSales = [
-    {
-    name: 'Jan',
+  {
+    name: "Jan",
     product1: 2000,
     product2: 2545,
-    },
+  },
 
-    {
-    name: 'Feb',
+  {
+    name: "Feb",
     product1: 4564,
     product2: 5863,
-    },
+  },
 
-    {
-    name: 'Mar',
+  {
+    name: "Mar",
     product1: 2536,
     product2: 1252,
-    },
+  },
 
-    {
-    name: 'Apr',
+  {
+    name: "Apr",
     product1: 2534,
     product2: 4246,
-    },
+  },
 
-    {
-    name: 'May',
+  {
+    name: "May",
     product1: 2364,
     product2: 4343,
-    },
+  },
 
-    {
-    name: 'June',
+  {
+    name: "June",
     product1: 4343,
     product2: 2443,
-    },
-   ]
+  },
+];
 const AreaChartComponent = () => {
-    return ( 
+  return (
     <ResponsiveContainer width="100%" height="100%">
-      <AreaChart 
-        width={500} 
-        height={400} 
+      <AreaChart
+        width={500}
+        height={400}
         data={productSales}
-        margin={{ right: 30}}
-      >   
+        margin={{ right: 30 }}
+      >
         <YAxis />
         <YAxis dataKey="name" />
         <CartesianGrid strokeDasharray="5 5" />
-        <Tooltip content={<CustomTooltip />}/>
+        <Tooltip content={<CustomTooltip />} />
         <Legend />
-        <Area 
+        <Area
           type="monotone"
-          dataKey="product1" 
+          dataKey="product1"
           stroke="#2363eb"
           fill="#3b82f6"
         />
-        <Area 
+        <Area
           type="monotone"
-          dataKey="product2" 
+          dataKey="product2"
           stroke="#2363eb"
           fill="#8b5cf6"
         />
       </AreaChart>
     </ResponsiveContainer>
-   
-     );
+  );
 };
 
 const CustomTooltip = ({ active, payload, label }) => {
-    if (active && payload && payload.length) {
-      return (
-        <div className="p-4 bg-slate-900 flex flex-col gap-4 rounded-md">
-          <p className="text-medium text-lg">{label}</p>
-          <p className="text-sm text-blue-400">
-            Product 1:
-            <span className="ml-2">${payload[1].value}</span>
-          </p>
-        </div>
-      );
-    }
+  if (active && payload && payload.length) {
+    return (
+      <div className="p-4 bg-slate-900 flex flex-col gap-4 rounded-md">
+        <p className="text-medium text-lg">{label}</p>
+        <p className="text-sm text-blue-400">
+          Product 1:
+          <span className="ml-2">${payload[1].value}</span>
+        </p>
+      </div>
+    );
+  }
 };
 export default AreaChartComponent;
--- rechart-examples/src/components/LineChart.jsx
+++ rechart-examples/src/components/LineChart.jsx
@@ -1,91 +1,89 @@
-'use client'
-
-import { LineChart, Line, YAxis, XAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, } from 'recharts'
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 const salesData = [
-    {
-    name: 'Jan',
+  {
+    name: "Jan",
     revenue: 2000,
     profit: 2545,
-    },
+  },
 
-    {
-    name: 'Feb',
+  {
+    name: "Feb",
     revenue: 4000,
     profit: 5863,
-    },
+  },
 
-    {
-    name: 'Mar',
+  {
+    name: "Mar",
     revenue: 2536,
     profit: 1252,
-    },
+  },
 
-    {
-    name: 'Apr',
+  {
+    name: "Apr",
     revenue: 2534,
     profit: 4246,
-    },
+  },
 
-    {
-    name: 'May',
+  {
+    name: "May",
     revenue: 2364,
     profit: 4343,
-    },
+  },
 
-    {
-    name: 'June',
+  {
+    name: "June",
     revenue: 4343,
     profit: 2443,
-    },
-   ]
+  },
+];
 const LineChartComponent = () => {
-    return ( 
-        <ResponsiveContainer width="100%" height="100%">
-        <LineChart 
-          width={500}
-          height={300}
-          data={salesData}
-          margin={{ 
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart
+        width={500}
+        height={300}
+        data={salesData}
+        margin={{
           right: 30,
-          }}
-        >   
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip content={<CustomTooltip />}/>
-          <Legend />
-          <Line
-            type="monotone"
-            dataKey="revenue" 
-            fill="#3b82f6"
-          />
-          <Line 
-            type="monotone"
-            dataKey="profit" 
-            fill="#8b5cf6"
-          />
-        </LineChart>
-      </ResponsiveContainer>
-     
-     );
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip content={<CustomTooltip />} />
+        <Legend />
+        <Line type="monotone" dataKey="revenue" fill="#3b82f6" />
+        <Line type="monotone" dataKey="profit" fill="#8b5cf6" />
+      </LineChart>
+    </ResponsiveContainer>
+  );
 };
 
 const CustomTooltip = ({ active, payload, label }) => {
-    if (active && payload && payload.length) {
-      return (
-        <div className="p-4 bg-slate-900 flex flex-col gap-4 rounded-md">
-          <p className="text-medium text-lg">{label}</p>
-          <p className="text-sm text-blue-400">
-            Revenue:
-            <span className="ml-2">${payload[0].value}</span>
-          </p>
-          <p className="text-sm text-indigo-400">
-            Profit:
-            <span className="ml-2">${payload[1].value}</span>
-          </p>
-        </div>
-      );
-    }
+  if (active && payload && payload.length) {
+    return (
+      <div className="p-4 bg-slate-900 flex flex-col gap-4 rounded-md">
+        <p className="text-medium text-lg">{label}</p>
+        <p className="text-sm text-blue-400">
+          Revenue:
+          <span className="ml-2">${payload[0].value}</span>
+        </p>
+        <p className="text-sm text-indigo-400">
+          Profit:
+          <span className="ml-2">${payload[1].value}</span>
+        </p>
+      </div>
+    );
+  }
 };
 export default LineChartComponent;
